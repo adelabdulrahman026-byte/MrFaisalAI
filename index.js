4278598764b06a210599e1a5fdc4aefb49eacf99
adelabdulrahman026-byte <adelabdulrahman026@gmail.com>
2026-07-23 15:37:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post("/webhook", async (req, res) => {
 
         // Gemini AI
         const model = genAI.getGenerativeModel({
-            model: "gemini-2.0-flash"
+            model: "Gemini 3.5 Flash Lite"
         });
 
 
